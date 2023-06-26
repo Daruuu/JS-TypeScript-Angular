3e91dba5d78419bb55858015a370f4536aaf9247
Daruuu <thedaruny16@gmail.com>
2023-06-26 21:00:56 +0200

    first commit

---
+++ js/basic01.js
@@ -0,0 +1,14 @@
+let humor = "ligero";
+console.log(humor);
+
+humor = 'oscuro';
+console.log(humor);
+
+// prompt("hello world!");
+
+/*
+function getPass(String passwd) {
+
+}*/
+
+console.log(Math.max(4, 6, 234, 65654));
