1fc6d7eb39e996a5e09d6ea79b570568e8081363
Daruuu <thedaruny16@gmail.com>
2023-07-03 18:53:33 +0200

    practice js basic fizzbuzz

--- js/exer00.js
+++ js/exer00.js
@@ -1,9 +1,38 @@
 //Escriba un ciclo que haga siete llamadas a console.log para generar el siguiente triángulo
 
-const hash = "#";
+
+/*
+let i = 1;
+while (i <= 7) {
+    console.log("#");
+    i += 1;
+}
+*/
+
+/*
+let item = '';
+
+for (let j = 0; j < 7; j++) {
+    item += '*';
+    console.log(item);
+}
+*/
 
 let i = 0;
+let emoticon = '';
 while (i < 7) {
-    i += 1;
-    console.log(hash);
+    i++;
+    emoticon += '*';
+    console.log(emoticon);
 }
+
+//FIZZBUZZ
+
+let i = 0;
+while (i < 100){
+    if (i % 3 === 0){
+        console.log('FIZZ');
+    }
+    i++;
+}
+
